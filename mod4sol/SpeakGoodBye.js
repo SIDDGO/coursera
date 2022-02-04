36fe431a531c3d7aab21aaefab268fa60902578f
Siddharth Goswami <98017334+SIDDGO@users.noreply.github.com>
2022-02-04 19:36:36 +0530

    Module 4 Solution

---
+++ mod4sol/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function(){
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function(name){
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
